a90f72b7347f9d357f11ac2c128889a490e288ff
Vinay-hack47 <rajputvinay1212@gmail.com>
2025-03-17 16:24:22 +0530

    updated cors option

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://expense-tracker-mearn-stack-frontend.vercel.app/"],
   credentials: true,
 }
 app.use(cors(corsOptions));
